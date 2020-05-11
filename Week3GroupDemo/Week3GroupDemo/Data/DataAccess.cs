d0d36e9d51b63932838d1a0620dea9e0af774a64
Ibrahim Jabr <ibrahim.aljabr93@gmail.com>
2020-05-11 11:12:02 -0500

    Week 3 Group Demo

---
+++ Week3GroupDemo/Week3GroupDemo/Data/DataAccess.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Week3GroupDemo.Data;
+using Week3GroupDemo.Models;
+
+namespace Week3GroupDemo.Data
+{
+    public  class DataAccess
+    {
+        private  readonly MvcGameContext DB ;
+
+        public  DataAccess(MvcGameContext context)
+        {
+            DB = context;
+        }
+
+        
+
+        public async  Task<List<Game>> gitIndex()
+        {
+           var d = await DB.Game.ToListAsync();
+            return d;
+        }
+
+        public  Game getGameD(int? id)
+        {
+            var game = DB.Game.Include(r => r.Rating)
+                .FirstOrDefault(m => m.Id == id);
+        
+            return game;
+        }
+
+        /*public static void creatGame(Game g)
+        {
+            g.Rating = DB.Rating.Where(r => r.Rating == g.Rating.Rating).FirstOrDefault();
+            if (ModelState.IsValid)
+            {
+                DB.Add(game);
+                DB.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+        }*/
+    }
+}
---
+++ Week3GroupDemo/Week3GroupDemo/Migrations/20200508190720_InitialCreatea.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Week3GroupDemo.Migrations
+{
+    public partial class InitialCreatea : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
